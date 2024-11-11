5d5666fd146635f46779fd21352c386c6649b698
riqalter <riqalter@gmail.com>
2024-11-11 10:37:06 +0700

    database and routes last progress

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PackagesController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "hello world";
 });
+
+Route::get('/packages', [PackagesController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
